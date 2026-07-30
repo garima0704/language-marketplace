a9b398772a444aca0def60753a91460670edf452
Garima Chouhan <garima1990chouhan@gmail.com>
2026-07-30 19:11:31 +0530

    Use router navigation for create channel button

--- src/components/channels/CreateChannelButton.tsx
+++ src/components/channels/CreateChannelButton.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
 export default function CreateChannelButton() {
+  const router = useRouter();
+
   return (
     <Button
-      asChild
+      onClick={() => router.push("/seller/channels/new")}
       className="bg-primary text-white hover:bg-primary hover:opacity-90 transition-opacity"
     >
-      <Link href="/seller/channels/new">Create Channel</Link>
+      Create Channel
     </Button>
   );
 }
