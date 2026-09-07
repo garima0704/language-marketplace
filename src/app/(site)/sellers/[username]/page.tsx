244dedc148bdde239e91cce652e76183914c5cd4
Garima Chouhan <garima1990chouhan@gmail.com>
2026-09-07 13:18:29 +0530

    Channel card erro resolved

--- src/app/(site)/sellers/[username]/page.tsx
+++ src/app/(site)/sellers/[username]/page.tsx
@@ -116,6 +116,8 @@ export default async function SellerProfilePage({
                 description: channel.description,
                 logo_url: channel.logo_url,
                 banner_url: channel.banner_url,
+                subscription_price: channel.subscription_price,
+                currency: channel.currency,
               }}
               seller={{
                 username: seller.username,
@@ -126,7 +128,7 @@ export default async function SellerProfilePage({
             />
 
           ))}
-
+          
         </div>
 
       </div>
